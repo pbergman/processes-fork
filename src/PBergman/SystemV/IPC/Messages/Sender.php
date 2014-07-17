68dafb9a9bf9b571d3fe659e0f9fbbcdeca52372
philip <pbergman@live.nl>
2014-07-17 10:35:31 +0200

    import class instead use of full namespace class

--- src/PBergman/SystemV/IPC/Messages/Sender.php
+++ src/PBergman/SystemV/IPC/Messages/Sender.php
@@ -6,6 +6,8 @@
 
 namespace PBergman\SystemV\IPC\Messages;
 
+use PBergman\SystemV\IPC\Helpers\ErrorsMapping;
+
 /**
  * Class Sender
  *
@@ -51,7 +53,7 @@ class Sender
     public function push()
     {
         if(false === $this->success = @msg_send($this->connection, $this->type, $this->data, $this->serialize, $this->blocking, $this->errorCode)) {
-            $this->error  = \PBergman\SystemV\IPC\Helpers\ErrorsMapping::getMessage($this->errorCode);
+            $this->error  = ErrorsMapping::getMessage($this->errorCode);
         } else {
             $this->status = msg_stat_queue($this->connection);
         }
