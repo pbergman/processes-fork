5f3505795b9d3b9af0b760e03a89b09fdab8814d
philip <pbergman@live.nl>
2014-08-14 13:18:13 +0200

    fix printing

--- src/PBergman/Fork/Work/Controller.php
+++ src/PBergman/Fork/Work/Controller.php
@@ -21,8 +21,6 @@ class Controller
 {
     /** @var int  */
     private $start;
-    /** @var bool */
-    private $isQuiet;
     /** @var Container|\PBergman\Fork\Output\Output[]|\PBergman\Fork\Helper\IdentifierHelper[]|\PBergman\Fork\Helper\ExitHelper[]|\PBergman\SystemV\IPC\Semaphore\Service[] */
     private $container;
 
@@ -43,43 +41,31 @@ class Controller
      */
     public function run(AbstractWork &$object)
     {
-        $this->write('Starting: %s', array($object->getName()));
+        $this->write('Starting: %s', array($object->getName()), !$object->isQuiet());
 
-        $this->preRun($object);
+        $object->setPid($this->container['helper.identifier']->getPid());
+
+        // Some pre-run setups
+        $this->checkTimeOut($object)->setExit($object);
 
         // Try execute child process
         try {
 
             $object->execute($this->container);
             $object->setDuration((microtime(true) - $this->start))
-                   ->setUsage(memory_get_usage());
+                ->setUsage(memory_get_usage());
 
             exit(0);
 
         } catch(\Exception $e) {
 
             $object->setSuccess(false)
-                   ->setError($e->getMessage())
-                   ->setDuration((microtime(true) - $this->start))
-                   ->setUsage(memory_get_usage());
+                ->setError($e->getMessage())
+                ->setDuration((microtime(true) - $this->start))
+                ->setUsage(memory_get_usage());
 
             exit($e->getCode());
         }
-    }
-
-
-
-    /**
-     * some pre-run calls before executing worker
-     *
-     * @param   AbstractWork $object
-     */
-    protected function preRun(AbstractWork &$object)
-    {
-        $object->setPid($this->container['helper.identifier']->getPid());
-        $this->isQuiet = $object->isQuiet();
-        $this->checkTimeOut($object);
-        $this->setExit($object);
     }
 
     /**
@@ -128,8 +114,8 @@ class Controller
             /** @var \PBergman\SystemV\IPC\Messages\Sender $sender */
             $sender = $this->container['queue.sender'];
             $sender->setData($object)
-                   ->setType(posix_getpid())
-                   ->push();
+                ->setType(posix_getpid())
+                ->push();
 
             if (false === $sender->isSuccess()) {
                 trigger_error(sprintf('Failed to send message, %s(%s)', $sender->getError(), $sender->getErrorCode()), E_USER_ERROR);
@@ -137,7 +123,7 @@ class Controller
             }
 
             // Print some debugging when finished
-            $this->write('Finished: %s (%s MB/%s s)', array($object->getName(), round($object->getUsage() /  1024 / 1024, 2), round($object->getDuration(), 2)));
+            $this->write('Finished: %s (%s MB/%s s)', array($object->getName(), round($object->getUsage() /  1024 / 1024, 2), round($object->getDuration(), 2)), !$object->isQuiet());
 
             // Release semaphore for queue
             $this->container['instance.semaphore']->release();
@@ -147,16 +133,17 @@ class Controller
     /**
      * helper to print some debugging
      *
-     * @param   $string
-     * @param   array $args
+     * @param   string  $string
+     * @param   array   $args
+     * @param   bool    $print
+     *
      * @return  \PBergman\Fork\Work\Controller
      */
-    protected function write($string, array $args = array())
+    protected function write($string, array $args = array(), $print = true)
     {
-        if (false !== $this->isQuiet) {
+        if ($print) {
             $this->container['output']->write((new LogFormatter(LogFormatter::PROCESS_CHILD))->format(vsprintf($string, $args)));
         }
-
         return $this;
     }
 }
