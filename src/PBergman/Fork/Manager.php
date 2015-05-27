65553e4765e8ae2ffddb41493819839aaeefb3b2
philip <pbergman@live.nl>
2015-05-27 19:57:13 +0200

    changd handling child process and small opt

--- src/PBergman/Fork/Manager.php
+++ src/PBergman/Fork/Manager.php
@@ -14,9 +14,18 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Processor as Processor;
 
-
+/**
+ * Class Manager
+ *
+ * @package PBergman\Fork
+ */
 class Manager
 {
+    const EVENT_FINISHED_WORKERS = 1;
+    const EVENT_SPAWN_CHILDREN = 2;
+    const EVENT_PUSH_JOBS = 3;
+    const EVENT_WATCH_CHILDREN = 4;
+    const EVENT_WAIT_FOR_FINISHED_CHILD = 5;
     const REDIS_PREFIX = '##MANAGER@';
     const EXIT_NORMAL = 0;
     const EXIT_TIMEOUT = 1;
@@ -37,7 +46,7 @@ class Manager
     /**
      * @param Redis $redis
      */
-    function __construct(Redis $redis = null, LoggerInterface $logger = null)
+    public function __construct(Redis $redis = null, LoggerInterface $logger = null)
     {
         if (is_null($redis)) {
             $this->redis = new Redis();
@@ -85,7 +94,6 @@ class Manager
         $this->jobs->attach($job);
         $this->jobs->rewind();
     }
-
     /**
      * main method to be called to start jobs,
      * will monitor children and delegate work
@@ -95,73 +103,127 @@ class Manager
     public function run()
     {
         $pids = [];
+        $event = self::EVENT_SPAWN_CHILDREN;
+        $signaled = [];
 
         do {
-            // Check for finished workers
-            if (count($pids) && $this->redis->lLen(self::parentChanelNotifier()) > 0) {
-                while (false !== $pid = $this->redis->lPop(self::parentChanelNotifier())) {
-                    if (isset($pids[$pid])) {
-                        $pids[$pid] = true;
-                    }
-                }
-            }
-            // Check if we got enough workers running
-            while (count($pids) < $this->workers) {
-                if (false === $this->jobs->valid()) {
-                    break;
-                }
-                if (false !== $pid = $this->fork()) {
-                    $this->logger->debug(sprintf('Child spawned %s [%s/%s]', $pid, count($pids) + 1, $this->workers));
-                    $pids[$pid] = true;
-                    $this->redis->reconnect();
-                } else {
-                    break;
-                }
-            }
-            // Check if work can be dispatched
-            while (array_sum($pids) > 0) {
-                $available = array_keys(array_filter($pids));
-                foreach ($available as $pid) {
-                    if ($this->jobs->valid()) {
-                        $this->pushJob($pid);
-                        if (isset($pids[$pid])) {
-                            $pids[$pid] = false;
-                        }
-                    } else {
-                        $this->pushJob($pid, true);
-                        if (isset($pids[$pid])) {
-                            unset($pids[$pid]);
+            switch ($event) {
+                case self::EVENT_FINISHED_WORKERS:
+                    if (count($pids) && $this->redis->lLen(self::parentChanelNotifier()) > 0) {
+                        while (false !== $pid = $this->redis->lPop(self::parentChanelNotifier())) {
+                            $this->logger->debug(sprintf('Child %s finished', $pid));
+                            if (isset($pids[$pid])) {
+                                $pids[$pid] = true;
+                            }
                         }
                     }
-                }
-            }
-            // Check exist and statuses from children
-            foreach ($pids as $pid => $working) {
-                $this->checkExitStatusChild($pid, $pids, $this->jobs->valid());
-            }
-            // no workers available, (blocking) wait for finished worker
-            if (array_sum($pids) === 0 && count($this->jobs) > 0) {
-                try {
-                    $pid = $this->redis->blPop([self::parentChanelNotifier()], 10);
-                    if (!empty($pid) && isset($pids[$pid[1]])) {
-                        $pids[$pid[1]] = true;
+                    $event++;
+                    break;
+                case self::EVENT_SPAWN_CHILDREN:
+                    if ($this->jobs->valid() && count($pids) < $this->workers) {
+                        $max = ($this->workers > count($this->jobs)) ? count($this->jobs) : $this->workers;
+                        for ($i = count($pids); $i < $max; $i++) {
+                            try {
+                                $pid = $this->fork();
+                                $this->logger->debug(sprintf('Child spawned %s [%s/%s]', $pid, count($pids) + 1, $this->workers));
+                                $pids[$pid] = true;
+                                $this->redis->reconnect();
+                            } catch (\Exception $e) {
+                                $this->logger->error(sprintf('%s @ %s(%s)', $e->getMessage(), $e->getFile(), $e->getLine()));
+                                exit(self::EXIT_ERROR);
+                            }
+                        }
                     }
-                } catch (\RedisException $r) {};
+                    $event++;
+                    break;
+                case self::EVENT_PUSH_JOBS:
+                    $available = array_keys(array_filter($pids));
+                    if (!empty($available)) {
+                        foreach ($available as $pid) {
+                            if ($this->jobs->valid()) {
+                                $this->pushJob($pid);
+                            } else {
+                                $this->pushJob($pid, true);
+                                $signaled[] = $pid;
+                            }
+                            if (isset($pids[$pid])) {
+                                $pids[$pid] = false;
+                            }
+                        }
+                    }
+                    $event++;
+                    break;
+                case self::EVENT_WATCH_CHILDREN:
+                    if (!empty($signaled)) {
+                        if ($this->jobs->valid() || count($this->getRunning($pids, $signaled))) {
+                            foreach ($pids as $pid => $working) {
+                                $this->checkExitChild($pid, $pids, $signaled, WNOHANG|WUNTRACED);
+                            }
+                        } else {
+                            $this->checkExitChild(-1, $pids, $signaled, WUNTRACED);
+                        }
+                    }
+                    $event++;
+                    break;
+                case self::EVENT_WAIT_FOR_FINISHED_CHILD:
+                    if (count($this->getRunning($pids, $signaled))) {
+                        try {
+                            $pid = $this->redis->blPop([self::parentChanelNotifier()], 10);
+                            if (!empty($pid) && isset($pids[$pid[1]])) {
+                                $pids[$pid[1]] = true;
+                            }
+                        } catch (\RedisException $r) {};
+                    }
+                    $event = self::EVENT_FINISHED_WORKERS;
+                    break;
+
             }
 
         } while (count($pids) > 0);
     }
 
-    protected function checkExitStatusChild($pid, array &$pids, $blocking)
+    /**
+     * helper to get all running pids that are not signaled for shutdown
+     *
+     * @param   array     $pids
+     * @param   array     $signaled
+     * @return  array
+     */
+    protected function getRunning(array $pids, array $signaled)
     {
-        $options = ($blocking) ? WNOHANG : WNOHANG|WUNTRACED;
+        $running = [];
 
+        array_walk($pids, function($v, $k) use (&$running, $signaled){
+            if (!$v && !in_array($k, $signaled)) {
+                $running[] = $k;
+            }
+        });
+
+        return $running;
+    }
+
+    /**
+     * do a check on child process(es) en output exit status
+     *
+     * @url http://php.net/manual/en/function.pcntl-waitpid.php
+     *
+     * @param int   $pid
+     * @param array $pids
+     * @param array $signaled
+     * @param int   $options
+     */
+    protected function checkExitChild($pid, array &$pids, &$signaled, $options = 0)
+    {
         if (0 !== $ret = pcntl_waitpid($pid, $status, $options)) {
             if ($ret === -1) {
-                unset($pids[$pid]);
                 $this->logger->error(sprintf('[%d] %s', $pid, pcntl_strerror(pcntl_get_last_error())));
             } else {
-                unset($pids[$ret]);
+                if (isset($pids[$ret])) {
+                    unset($pids[$ret]);
+                }
+                if (in_array($ret, $signaled)) {
+                    unset($signaled[array_search($ret, $signaled)]);
+                }
                 switch (true) {
                     case pcntl_wifstopped($status):
                         $this->logger->error(sprintf('Signal: %s caused this child to stop', pcntl_wstopsig($status)));
@@ -172,7 +234,6 @@ class Manager
                     default:
                         $this->logger->debug(sprintf("Child %s exited with code %s", $ret, pcntl_wexitstatus($status)));
                 }
-
             }
         }
     }
@@ -186,15 +247,22 @@ class Manager
     {
         if ($exit) {
             $this->logger->debug(sprintf('Pushing exit signal to %s%s', self::REDIS_PREFIX, $pid));
-            $this->redis->publish(self::childChanelNameExit($pid, null), true);
+            if (false === $this->redis->publish(self::childChanelNameExit($pid, null), true)) {
+                $this->logger->error($this->redis->getLastError());
+                $this->redis->clearLastError();
+            }
         } else {
             $this->logger->debug(sprintf('Pushing job to queue %s%s', self::REDIS_PREFIX, $pid));
             $this->jobs->rewind();
             $job = $this->jobs->current();
             $this->jobs->detach($this->jobs->current());
             $message = $this->generator->pack($job);
-            $this->redis->publish(self::childChanelNameWork($pid, null), $message );
+            if (false === $this->redis->publish(self::childChanelNameWork($pid, null), $message)) {
+                $this->logger->error($this->redis->getLastError());
+                $this->redis->clearLastError();
+            }
         }
+
     }
 
     /**
@@ -223,29 +291,39 @@ class Manager
                         $redis = $client->duplicate();
                         switch ($chan) {
                             case self::childChanelNameWork():
-                                if (null !== $work = $this->generator->unpack($msg)) {
-                                    if (is_callable($work)) {
-                                        try{
-                                            $work($redis, $this->logger);
-                                            $this->logger->debug('Job finished');
-                                        } catch (\Exception $e){
-                                            $this->logger->error(sprintf('%s: %s', get_class($e), $e->getMessage()));
+                                try {
+                                    $work = $this->generator->unpack($msg);
+                                    if (!is_null($work )) {
+                                        if (is_callable($work)) {
+                                            try{
+                                                $work($redis, $this->logger);
+                                                $this->logger->debug('Job finished');
+                                            } catch (\Exception $e){
+                                                $this->logger->error(sprintf('%s: %s', get_class($e), $e->getMessage()));
+                                            }
+                                            if (!$redis->publish(self::childChanelNameCleanup(null, null), true)) {
+                                                $this->logger->error($redis->getLastError());
+                                            }
+                                        }  else {
+                                            $this->logger->notice(sprintf('Received work but was not callable, got: "%s"', gettype($work)));
                                         }
-                                        if (function_exists('gc_collect_cycles')) {
-                                            $this->logger->debug('Cleaning up resources');
-                                            gc_collect_cycles();
-                                        }
-                                        if (!$redis->lPush(self::parentChanelNotifier(), getmypid())) {
-                                            $this->logger->error($redis->getLastError());
-                                        }
-                                    }  else {
-                                        $this->logger->notice(sprintf('Received work but was not callable, got: "%s"', gettype($work)));
                                     }
+                                } catch (\Exception $e) {
+                                    $this->logger->error(sprintf('%s @ %s(%s)', $e->getMessage(), $e->getFile(), $e->getLine()));
+                                }
+                                break;
+                            case self::childChanelNameCleanup():
+                                if (function_exists('gc_collect_cycles')) {
+                                    $this->logger->debug('Cleaning up resources');
+                                    gc_collect_cycles();
+                                }
+                                if (!$redis->lPush(self::parentChanelNotifier(), getmypid())) {
+                                    $this->logger->error($redis->getLastError());
                                 }
                                 break;
                             case self::childChanelNameExit():
                                 $this->logger->debug('Received exit signal, shutting down');
-                                exit(self::EXIT_NORMAL);
+                                $client->close();
                                 break;
                         }
                     });
